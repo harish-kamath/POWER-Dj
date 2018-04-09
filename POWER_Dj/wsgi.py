e4cdb56b65d1d5cba2d460dbb5ead25b526c6c17
Harish Kamath <harish.k.kamath@gmail.com>
2018-04-09 19:13:31 -0400

    eb wsgi change

--- POWER_Dj/wsgi.py
+++ POWER_Dj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "POWER_Dj.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "ayflare.settings"
 
 application = get_wsgi_application()
